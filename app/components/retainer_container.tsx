e3ae47f07546c866e88810f7102b971d53c56048
chrubble <47755013+notchrubble@users.noreply.github.com>
2025-12-05 14:46:56 -0800

    implemented extremely preliminary scaffolding

---
+++ app/components/retainer_container.tsx
@@ -0,0 +1,7 @@
+export default function RetainerContainer() {
+  return (
+    <div className="col-span-4 flex min-h-screen items-center justify-center rounded-2xl bg-zinc-800 font-sans outline dark:bg-black">
+      <h3>Retainers</h3>
+    </div>
+  );
+}
