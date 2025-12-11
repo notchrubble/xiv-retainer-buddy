d275236c9a2890c99bf0af1312f8121c93edf9f8
chrubble <47755013+notchrubble@users.noreply.github.com>
2025-12-11 14:26:53 -0800

    refactor: create AddRetainerButton component and update RetainerContainer

--- app/components/item_list.tsx
+++ app/components/item_list.tsx
@@ -1,6 +1,6 @@
 export default function ItemList() {
   return (
-    <div className="col-span-8 flex min-h-screen items-center justify-center rounded-2xl font-sans outline">
+    <div className="col-span-12 flex min-h-screen items-center justify-center rounded-2xl font-sans outline lg:col-span-8">
       <h3>Items</h3>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import ItemList from "./components/item_list";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center px-16 py-32 sm:items-start">
+      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center px-4 py-8 lg:items-start lg:px-16 lg:py-32">
         <h1 className="text-2xl font-bold text-zinc-50 dark:text-white">
           FFXIV Retainer Buddy
         </h1>
         <p className="mt-4 text-zinc-400 dark:text-zinc-500">
           A tool to help you maximize your gil gains
         </p>
-        <div className="mt-10 grid w-full grid-cols-12 gap-8">
+        <div className="mt-10 grid w-full grid-cols-12 gap-0 lg:gap-8">
           <RetainerContainer></RetainerContainer>
           <ItemList></ItemList>
         </div>
